4cf96c1a78b5c9540dcb38fb81d916dae4cdd4b0
AArtlone <artem.stolyha@gmail.com>
2019-02-13 18:57:23 +0100

    Created base tank class and shooting mechanic
    Co-authored-by: Super-Lovers <n.v.ivanov@st.hanze.nl>

---
+++ ElectivesAI/Assets/Scripts/Niq.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Niq : Tank
+{
+    public Transform shootingPos; // a reference to the position at which the bullet is instantiated
+
+    public override void Shoot(Transform shootingPos)
+    {
+        shootingPos = this.shootingPos; //assigning the shooting position to the current tank's shooting position
+        base.Shoot(shootingPos);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.N))
+        {
+            Shoot(shootingPos);
+        }
+    }
+}
---
+++ ElectivesAI/Assets/Scripts/Tank.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour 
+{
+    #region Default tank variables
+    private float _health;
+
+    private float _movementSpeed;
+    private float _rotationSpeed;
+
+    private int _ammo;  
+    private float _fireRate;
+    private float _fireRange;
+    private float _bulletSpeed = 1000f;
+    #endregion
+
+    #region References for the shooting function
+    public GameObject BulletPrefab; // A reference to the the bullet object prefab
+    public GameObject CannonHead;  // A reference to the canon head from which the bullet is being shot from
+    #endregion
+
+    public virtual void Shoot(Transform shootingPos)
+    {
+        // An instance of a bullet and setting it a child of a shooting position
+        GameObject bullet = Instantiate(BulletPrefab, shootingPos.position, Quaternion.identity, shootingPos);
+
+        // Ignore the collision between the bullet and a cannon head because the bullet comes out of the cannon.
+        Physics.IgnoreCollision(bullet.GetComponent<Collider>(), CannonHead.GetComponent<Collider>());
+
+        // Adding force to the bullet
+        bullet.GetComponent<Rigidbody>().AddForce(shootingPos.forward * _bulletSpeed * 20 * Time.deltaTime);
+    }
+}
